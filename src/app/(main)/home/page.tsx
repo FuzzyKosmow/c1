bcc4ee56555b9ddafbded10f412779fd1496c487
Shoru <bapcui596@gmail.com>
2024-12-16 09:18:48 +0700

    misc: redirect on filter

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,17 +1,15 @@
 import BestDealSection from "@/components/BestDealSection";
-import BestSellerSection from "@/components/BestSellerSection";
 import HomeCover from "@/components/HomeCover";
 import NewArrivalSection from "@/components/NewArrivalSection";
-import PopularBrandSection from "@/components/PopularBrandSection";
+import FeaturedProductSection from "@/components/FeaturedProductSection";
 
 export default function Page() {
-	return (
-		<div className=" flex flex-col gap-12 px-24 py-10">
-			<HomeCover />
-			<PopularBrandSection />
-			<BestDealSection />
-			<NewArrivalSection />
-			<BestSellerSection />
-		</div>
-	);
+  return (
+    <div className=" flex flex-col gap-12 px-24 ">
+      <HomeCover />
+      <FeaturedProductSection />
+      <NewArrivalSection />
+      <BestDealSection />
+    </div>
+  );
 }
--- src/app/(main)/souvenir/page.tsx
+++ src/app/(main)/souvenir/page.tsx
@@ -1,5 +1,72 @@
-import { redirect } from "next/navigation";
+import { CheckBox, CheckBoxGroup } from "@/components/CheckBox";
+import ProductItem from "@/components/ProductItem";
+import Text from "@/components/Typography/Text";
+import { getProductListAPI } from "@/services/api/product/product-list";
+import Link from "next/link";
 
-export default function Page() {
-  redirect("/home/product-list");
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string | undefined };
+}) {
+  const category = decodeURIComponent((await searchParams)?.category || "");
+  const country = (await searchParams)?.country;
+  const relatedCity = (await searchParams)?.relatedCity;
+  const sortType = (await searchParams)?.sort;
+
+  console.log("category", category);
+  console.log("country", country);
+  console.log("city", relatedCity);
+
+  const productResponse = await getProductListAPI({
+    category: category,
+    country: country,
+    relatedCity: relatedCity,
+    sort: sortType,
+  });
+
+  const productList = productResponse.products as IProduct[];
+
+  return (
+    <div className=" px-24 pt-10">
+      <div className=" bg-secondary-200 rounded-md px-5 py-2 flex items-center">
+        <Text className=" mr-16 font-semibold">Sort by: </Text>
+        <CheckBoxGroup selected={sortType} className=" flex gap-5">
+          <Link
+            href={`/home/product-list?sort=${encodeURIComponent(
+              "price-low-to-high"
+            )}`}
+          >
+            <CheckBox title="Price: Low to High" name="price-low-to-high" />
+          </Link>
+          <Link
+            href={`/home/product-list?sort=${encodeURIComponent(
+              "price-high-to-low"
+            )}`}
+          >
+            <CheckBox title="Price: High to Low" name="price-high-to-low" />
+          </Link>
+          <Link
+            href={`/home/product-list?sort=${encodeURIComponent(
+              "most-popular"
+            )}`}
+          >
+            <CheckBox title="Most popular" name="most-popular" />
+          </Link>
+          <Link
+            href={`/home/product-list?sort=${encodeURIComponent(
+              "newest-arrival"
+            )}`}
+          >
+            <CheckBox title="Newest arrival" name="newest-arrival" />
+          </Link>
+        </CheckBoxGroup>
+      </div>
+      <div className=" mt-10 grid grid-cols-6 gap-5">
+        {productList.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
